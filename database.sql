5e6ec84fd32bf14ab08e7e41c7314d290480577f
Mehul Agarwal <agarwalmehul423@gmail.com>
2023-10-27 18:03:24 +0530

    changes

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `flight_ticket_booking_system` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `flight_ticket_booking_system`;
 -- MySQL dump 10.13  Distrib 8.0.34, for macos13 (arm64)
 --
 -- Host: localhost    Database: flight_ticket_booking_system
@@ -111,4 +113,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-27 17:54:17
+-- Dump completed on 2023-10-27 18:02:31
